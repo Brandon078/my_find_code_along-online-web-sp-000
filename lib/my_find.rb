3b5f2fa8b83ff2345476661a30adaff89b063860
brandon moro <brandon_078@hotmail.com>
2019-12-05 03:38:12 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -6,7 +6,7 @@ def my_all?(collection)
   while i < collection.length
     return_values << yield(collection[i])
     i += 1
-    binding.pry 
+    binding.pry
   end
   if return_values.include?(false)
     false
